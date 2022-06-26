a6ab1d3d632690c36ef8791361a39b1c4c6ac4d8
David Prada Linarez <davidprali27@gmail.com>
2022-06-26 12:31:49 -0500

    gateway

--- gateway-service/src/main/java/com/gateway/service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/gateway/service/GatewayServiceApplication.java
@@ -3,10 +3,11 @@ package com.gateway.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableEurekaClient
-
+@CrossOrigin(origins = "*")
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
