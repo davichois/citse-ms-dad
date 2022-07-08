e567ea67fd0d5555c1fd37f5ec5af6b87fc0fd32
Omar guerrero <omarguerrero@upeu.edu.pe>
2022-07-07 23:26:11 -0500

    test 003

--- citse-ms-sg/src/main/java/com/sg/citse/repository/UsuarioRepository.java
+++ citse-ms-sg/src/main/java/com/sg/citse/repository/UsuarioRepository.java
@@ -3,7 +3,9 @@ package com.sg.citse.repository;
 import com.sg.citse.entity.Usuario;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "usuarios")
 public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, Integer> {
@@ -12,5 +14,8 @@ public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, I
 
     @Query("select u from Usuario u where u.nuUsuario=?1")
     public Usuario obtenerPorNuUsuario(String nuUsuario);
+
+    @RestResource(path = "estado")
+    public Usuario findByEsUsuario(@Param("is") Boolean estado);
 
 }
